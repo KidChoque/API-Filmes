c732e399ae53fa026c6a8215ea6eae45a50bde68
KidChoque <lucassmachado1998@gmail.com>
2023-08-23 10:58:28 -0300

    Corrigindo erros

---
+++ webapi.filmes.manha.Lucas/Controllers/GeneroController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi.filmes.manha.Lucas.Domains;
+using webapi.filmes.manha.Lucas.Interface;
+using webapi.filmes.manha.Lucas.Repositorys;
+
+namespace webapi.filmes.manha.Lucas.Controllers
+{
+    //Definie que a rota de uma requisição será no seguinte formato
+    //domínio/api/nomeController
+    //ex: http://localhost:5000/api/genero
+
+    [Route("api/[controller]")]
+
+    //Define que é um controlador de API
+    [ApiController]
+
+    //Define que o tipo de resposta da API será no formato JSON
+    [Produces("application/json")]
+
+    //Método controlador que herda da controller base
+    //Onde será criado os Endpoints(Rotas)
+
+    public class GeneroController : ControllerBase
+    {
+        /// <summary>
+        /// Objeto _generoRepository que irá receber todos os métodos definidos na Interface IGeneroRepository
+        /// </summary>
+        private IGeneroRepository _generoRepository { get; set; }
+
+        /// <summary>
+        /// Instancia o objeto _generoRepository para que haja referência aos métodos no repositório 
+        /// </summary>
+        public GeneroController() 
+        {
+            _generoRepository = new GeneroRepository();
+        }
+
+        /// <summary>
+        /// Endpoint que aciona o método ListarTodos no repositório
+        /// </summary>
+        /// <returns>Resposta para o usuário(front-end)</returns>
+        [HttpGet]
+
+
+
+        public IActionResult ListarGenero()
+        {
+
+            try
+            {
+                //Cria uma lista que recebe os dados da requisição 
+                List<GeneroDomain> ListaGenero = _generoRepository.ListarTodos();
+
+                //Retorna a lista do formato JSON com o status code OK(200)
+                return Ok(ListaGenero);
+            }
+
+            catch (Exception erro)
+            {
+                //Retorna um status code BadRequest(400) e a mensagem do erro
+                return BadRequest(erro.Message);
+            }
+
+
+           
+        }
+
+
+    }
+}
--- webapi.filmes.manha.Lucas/Repositorys/GeneroRepository.cs
+++ webapi.filmes.manha.Lucas/Repositorys/GeneroRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.HttpOverrides;
+using System.Data.SqlClient;
 using webapi.filmes.manha.Lucas.Domains;
 using webapi.filmes.manha.Lucas.Interface;
 using static System.Runtime.InteropServices.JavaScript.JSType;
@@ -7,6 +8,40 @@ namespace webapi.filmes.manha.Lucas.Repositorys
 {
     public class GeneroRepository : IGeneroRepository
     {
+        private string StringConexao = "Data Source = NOTE08-S14; Initial Catalog = Filmes; User ID = SA; Pwd=senai@134;";
+
+        public List<GeneroDomain> ListarTodos()
+        {
+            List<GeneroDomain> ListaGenero = new List<GeneroDomain>();
+
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                string querySelectAll = "SELECT IdGenro,Nome FROM Genero";
+
+                con.Open();
+
+                SqlDataReader rdr;
+
+                using (SqlCommand cmd = new SqlCommand(querySelectAll))
+                {
+                    rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        GeneroDomain genero = new GeneroDomain()
+                        {
+                            IdGenero = Convert.ToInt32(rdr[0]),
+
+                            Nome = rdr["Nome"].ToString()
+                        };
+
+                        ListaGenero.Add(genero);
+                    }
+                return ListaGenero;
+                }
+
+            }
+        }
         public void AtualizarIdCorpo(GeneroDomain genero)
         {
             throw new NotImplementedException();
@@ -32,11 +67,8 @@ namespace webapi.filmes.manha.Lucas.Repositorys
             throw new NotImplementedException();
         }
 
-        public List<GeneroDomain> ListarTodos()
-        {
-            throw new NotImplementedException();
-        }
+      
 
-        private string StringConexao = "Data Source = NOTE08-S14; Initial Catalog = Filmes; User ID = SA; Pwd=senai@134"; 
+    
     }
 }
